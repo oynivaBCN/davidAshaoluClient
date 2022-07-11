a23ed29836b01fb93f30b42d401372c0f7489c99
oynivaBCN <pjknocker@yahoo.com>
2022-07-11 12:47:20 +0200

    feat: minor changes

--- app/src/routes/router.js
+++ app/src/routes/router.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { RoutesLinks } from './routes-links';
+import PrivateRoute from './private-route';
 
 import Home from '../features/home';
 import Dashboard from "../features/dashboard"
 import Login from '../features/login';
 import Register from "../features/register"
 
-import PrivateRoute from './private-route';
 
 const Router = () => {
 	return (
