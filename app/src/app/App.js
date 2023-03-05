fe056d824f0c9bb2b4249e29fb151f8fbc82d281
pandorajk <pjknocker@yahoo.com>
2023-03-05 22:01:12 +0100

    feat: login, logout, refreshToken and redux

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -1,17 +1,34 @@
+import React, { useCallback, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import SessionStatus from '../features/session-status';
-import Header from '../features/header'
-import { AuthProvider } from '../hooks/useAuth';
-import Router from "../routes/router";
+import Header from '../features/header';
+import Router from '../routes/router';
+import UserOperations from '../redux/user/user-operations';
 
 function App() {
-  return (
-        <AuthProvider>
-          <Header />
-          <SessionStatus />
-          <Router />
-        </AuthProvider>
-  );
+	const location = useLocation();
+	const tokens = useSelector((state) => state.tokens);
+	const dispatch = useDispatch();
+
+	const refreshToken = useCallback(async (tokens) => dispatch(UserOperations.refreshToken(tokens)), [dispatch]);
+
+	const handleRouteChange = useCallback(async () => {
+		if (tokens) {
+			await refreshToken(tokens);
+		}
+	}, [tokens, refreshToken]);
+
+	useEffect(() => {
+		handleRouteChange(location.pathname);
+	}, [location.pathname, handleRouteChange]);
+
+	return (
+		<>
+			<Header />
+			<Router />
+		</>
+	);
 }
 
 export default App;
--- app/src/features/login/login-view.js
+++ app/src/features/login/login-view.js
@@ -1,69 +1,76 @@
-import React, {useState} from 'react';
-import { useAuth } from "../../hooks/useAuth"
-import { useNavigate } from "react-router-dom"
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import UserOperations from '../../redux/user/user-operations';
 import { RoutesLinks } from '../../routes/routes-links';
+import SessionService from '../../services/session-service.js';
 
-const LoginForm = ()=>{
-    let navigate = useNavigate();
-    const [state , setState] = useState({
-        username: "",
-        password : ""
-    })
+const LoginForm = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-    const { authenticate } = useAuth()
+	const [state, setState] = useState({
+		username: '',
+		password: '',
+	});
 
-    const handleChange = (e) => {
-        const {id , value} = e.target   
-        setState(prevState => ({
-            ...prevState,
-            [id] : value
-        }))
-    }
+	const handleChange = (e) => {
+		const { id, value } = e.target;
+		setState((prevState) => ({
+			...prevState,
+			[id]: value,
+		}));
+	};
 
-    const handleSubmitClick = (e) => {
-        e.preventDefault();
-        authenticate(state.username, state.password).then(data => {
-            navigate(RoutesLinks.DASHBOARD)
-            console.log("Logged in!", data)
-        }).catch(err => {
-            console.error("Failed to log in!", err)
-        })
-    }
+	const setAuthInfo = (data) => {
+		dispatch(UserOperations.setAuthInfo(data));
+	};
 
-  return(
-    <>
-        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
-            <form onSubmit={handleSubmitClick}>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleUserName">Username</label>
-                    <input type="text" 
-                        className="form-control" 
-                        id="username" 
-                        aria-describedby="usernamehelp" 
-                        placeholder="Enter Username"
-                        onChange={handleChange}
+	const setUserInfo = (data) => {
+		dispatch(UserOperations.setUserInfo(data));
+	};
 
-                    />
-                </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" 
-                        className="form-control" 
-                        id="password" 
-                        placeholder="Password"
-                        onChange={handleChange}
-                    />
-                </div>
-                <button 
-                    type="submit" 
-                    className="btn btn-primary"
-                >
-                    Sign In
-                </button>
-            </form>
-        </div>
-        </>
-    )
-}
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		try {
+			const { data } = await SessionService.login(state.username, state.password);
+			setAuthInfo(data?.tokens);
+			setUserInfo(data?.user);
+			if (data?.tokens?.access_token) {
+				navigate(RoutesLinks.DASHBOARD);
+			}
+		} catch (error) {
+			console.error(error);
+			alert(error.response.data?.name);
+		}
+	};
 
-export default LoginForm
+	return (
+		<>
+			<div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+				<form onSubmit={handleLogin}>
+					<div className="form-group text-left">
+						<label htmlFor="exampleUserName">Username</label>
+						<input
+							type="text"
+							className="form-control"
+							id="username"
+							aria-describedby="usernamehelp"
+							placeholder="Enter Username"
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="form-group text-left">
+						<label htmlFor="exampleInputPassword1">Password</label>
+						<input type="password" className="form-control" id="password" placeholder="Password" onChange={handleChange} />
+					</div>
+					<button type="submit" className="btn btn-primary">
+						Sign In
+					</button>
+				</form>
+			</div>
+		</>
+	);
+};
+
+export default LoginForm;
